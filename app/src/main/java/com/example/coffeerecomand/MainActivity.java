562c70dd46693c399c26f32e71266a82d29f16f8
Ogamdo <BellOfJerusalem@proton.me>
2024-11-20 21:47:04 +0900

    이미지의 파일 오류 수정 ->coffeback이란 이름으로 다시 파일을 폴더안에 붙여넣기함 menu에 대해 배운 것을 기말고사 프로젝트에 응용하기로 함. (커피 테스트 어플의 특성상 메뉴가 필요하다고 판단) menu와 관련된 메소드 오버라이드를 MainActivity에 추가함

--- app/src/main/java/com/example/coffeerecomand/MainActivity.java
+++ app/src/main/java/com/example/coffeerecomand/MainActivity.java
@@ -1,26 +1,44 @@
 package com.example.coffeerecomand;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+;import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    View menuView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        Button.
+
+
+
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // defind menu items
+        int id = item.getItemId();
+        if (id == R.id.restart) {
+            Toast.makeText(this, "커피 추천 테스트를 다시 시작합니다.", Toast.LENGTH_SHORT).show();
+
+            return true;
+        }
+        else if (id == R.id.coffeMenus) {
+            Toast.makeText(this, "추천 해주는 커피 메뉴의 후보군을 봅니다.", Toast.LENGTH_SHORT).show();
+        } else if (id==R.id.justSee) {
+            Toast.makeText(this, "이 곳에는 뭔가 귀여운 것을 띄워보려고 합니다. 이 곳에 띄우는 이미지나 모델링의 캐릭터가, 커피 추천 테스트의 결과를 알려주게 할 것입니다.", Toast.LENGTH_SHORT).show();
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
