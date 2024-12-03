684ae2625812556f1428794e25dda49f6bdab657
Ogamdo <BellOfJerusalem@proton.me>
2024-12-03 15:07:06 +0900

    각종 이미지 추가 및 단순한 버튼에서 커스텀 버튼으로 수정(activity_main.xml) 한 클래스 내에 모든 기능을 구현하려는 시도를 했음.

--- app/src/main/java/com/example/coffeerecomand/MainActivity.java
+++ app/src/main/java/com/example/coffeerecomand/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.coffeerecomand;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,18 +8,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     String[] questions = {getString(R.string.question0),getString(R.string.question1),getString(R.string.question2), getString(R.string.question3), getString(R.string.question4)};
+
     View menuView;
     Button yesBtn;
     Button noBtn;
+    TextView questionText;
+    int currentIndex=0;
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        questionText = findViewById(R.id.questionText);
+        yesBtn = findViewById(R.id.yesBtn);
+        noBtn = findViewById(R.id.noBtn);
+
+        yesBtn.setOnClickListener(view -> {
+            currentIndex++;
+
+        });
+// 첫 번째 질문 표시
+        updateQuestion(currentIndex);
+
 
 
     }
@@ -43,5 +60,11 @@ public class MainActivity extends AppCompatActivity {
 
         }
         return super.onOptionsItemSelected(item);
+    }
+    void updateQuestion(int index){
+
+        if(index<5) {
+            questionText.setText(questions[index]);
+        }
     }
 }
